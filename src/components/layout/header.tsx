e25bdc27053e83f285ba2c7eff5627f742c0066a
mjost <marius.jost@inovex.de>
2023-09-25 14:56:36 +0200

    merge main

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -20,25 +20,25 @@ export default component$(() => {
         {userSession.isLoggedIn ? (
           <ul class="nav navbar-nav pull-xs-right">
             <li class="nav-item">
-              <Link class={`nav-link ${pathname.value == "/" ? "active" : ""}`} href="/">
+              <Link class={`nav-link ${pathname.value == "/" ? "active" : ""}`} href="/" id="home">
                 Home
               </Link>
             </li>
 
             <li class="nav-item">
-              <Link class={`nav-link ${pathname.value === "/editor/" ? "active" : ""}`} href="/editor">
+              <Link class={`nav-link ${pathname.value === "/editor/" ? "active" : ""}`} href="/editor" id="editor">
                 <i class="ion-compose"></i>&nbsp;New Article{" "}
               </Link>
             </li>
 
             <li class="nav-item">
-              <Link class={`nav-link ${pathname.value === "/setting/" ? "active" : ""}`} href="/settings">
+              <Link class={`nav-link ${pathname.value === "/setting/" ? "active" : ""}`} href="/settings" id="settings">
                 {" "}
                 <i class="ion-gear-a"></i>&nbsp;Settings{" "}
               </Link>
             </li>
             <li class="nav-item">
-              <Link class={`nav-link ${pathname.value.includes("profile") ? "active" : ""}`} href={`/profile/${userSession.username}`}>
+              <Link class={`nav-link ${pathname.value.includes("profile") ? "active" : ""}`} href={`/profile/${userSession.username}`} id="profile">
                 {userSession.image && <img width={25} height={25} src={userSession.image} class="user-pic" />}
                 {userSession.username}
               </Link>
@@ -47,17 +47,17 @@ export default component$(() => {
         ) : (
           <ul class="nav navbar-nav pull-xs-right">
             <li class="nav-item">
-              <Link class={`nav-link ${pathname.value === "/" ? "active" : ""}`} href="/">
+              <Link class={`nav-link ${pathname.value === "/" ? "active" : ""}`} href="/" id="home">
                 Home
               </Link>
             </li>
             <li class="nav-item">
-              <Link class={`nav-link ${pathname.value === "/login/" ? "active" : ""}`} href="/login">
+              <Link class={`nav-link ${pathname.value === "/login/" ? "active" : ""}`} href="/login" id="login">
                 Sign in
               </Link>
             </li>
             <li class="nav-item">
-              <Link class={`nav-link ${pathname.value === "/register/" ? "active" : ""}`} href="/register">
+              <Link class={`nav-link ${pathname.value === "/register/" ? "active" : ""}`} href="/register" id="register">
                 Sign up
               </Link>
             </li>
