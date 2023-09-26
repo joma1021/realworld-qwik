a614857e41b96152601007aee67763ce3aa9061f
mjost <marius.jost@inovex.de>
2023-09-26 16:36:40 +0200

    update home page nav

--- src/components/tabs/feed-tabs.tsx
+++ src/components/tabs/feed-tabs.tsx
@@ -1,56 +1,39 @@
-import type { PropFunction } from "@builder.io/qwik";
-import { component$, useTask$, useContext } from "@builder.io/qwik";
-import { Tab } from "~/models/tab";
-import type { OverviewStore } from "../articles/article-overview";
-import { Link } from "@builder.io/qwik-city";
+import { component$, useTask$, useContext, useSignal } from "@builder.io/qwik";
+
+import { Link, useLocation } from "@builder.io/qwik-city";
 import type { UserSessionStore } from "../../common/auth/auth-provider";
 import { UserSessionContext } from "../../common/auth/auth-provider";
+import { Tab } from "~/models/tab";
 
-interface FeedTabsProps {
-  overviewStore: OverviewStore;
-  updateTag$: PropFunction<(tag: string) => void>;
-  updateTab$: PropFunction<(tab: Tab) => void>;
-}
-
-export default component$((props: FeedTabsProps) => {
+export default component$(() => {
   const userSession = useContext<UserSessionStore>(UserSessionContext);
+  const { url } = useLocation();
+  const tab = useSignal(userSession.isLoggedIn ? Tab.Your : Tab.Global);
+
   useTask$(({ track }) => {
-    track(() => props.overviewStore.selectedTag);
-    if (props.overviewStore.selectedTag != "") props.updateTab$(Tab.Tag);
+    track(() => url);
+    const filter = url.searchParams.get("filter");
+    if (filter) tab.value = filter;
   });
   return (
     <div class="feed-toggle">
       <ul class="nav nav-pills outline-active">
         <li class="nav-item">
           {userSession.isLoggedIn && (
-            <Link
-              class={`nav-link ${props.overviewStore.activeTab == Tab.Your && "active"}`}
-              style="cursor: pointer;"
-              onClick$={async () => {
-                await props.updateTab$(Tab.Your);
-                await props.updateTag$("");
-              }}
-            >
+            <Link class={`nav-link ${tab.value == Tab.Your && "active"}`} style="cursor: pointer;" href={`/?filter=${Tab.Your}`}>
               Your Feed
             </Link>
           )}
         </li>
         <li class="nav-item">
-          <Link
-            class={`nav-link ${props.overviewStore.activeTab == Tab.Global && "active"}`}
-            style="cursor: pointer;"
-            onClick$={async () => {
-              await props.updateTab$(Tab.Global);
-              await props.updateTag$("");
-            }}
-          >
+          <Link class={`nav-link ${tab.value == Tab.Global && "active"}`} style="cursor: pointer;" href={`/?filter=${Tab.Global}`}>
             Global Feed
           </Link>
         </li>
-        {props.overviewStore.activeTab == Tab.Tag && (
+        {tab.value != Tab.Your && tab.value != Tab.Global && (
           <li class="nav-item">
-            <Link class={`nav-link ${props.overviewStore.activeTab == Tab.Tag && "active"}`} style="cursor: pointer;">
-              #{props.overviewStore.selectedTag}
+            <Link class={"nav-link active"} style="cursor: pointer;">
+              #{tab.value}
             </Link>
           </li>
         )}
